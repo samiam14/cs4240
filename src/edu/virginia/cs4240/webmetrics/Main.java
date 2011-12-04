def95308a98a829ff88ae40a7493b120d59fcdfa
Sam Eberspacher <samiam14@gmail.com>
2011-12-04 18:16:13 -0500

    project file structure commit

---
+++ src/edu/virginia/cs4240/webmetrics/Main.java
@@ -0,0 +1,12 @@
+package edu.virginia.cs4240.webmetrics;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Project up and running");
+	}
+
+}
